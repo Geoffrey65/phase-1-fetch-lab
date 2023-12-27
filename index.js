74437770fcfd5683bb461e253a28c8a94276ceb4
Geoffrey <geoffrey>
2023-12-27 15:27:33 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+import { renderBooks } from './renderBooks.js'; 
+function fetchBooks() {
+  
+  const fetchRequest = fetch('https://anapioficeandfire.com/api/books');
+
+ 
+  return fetchRequest.then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not OK');
+    }
+    return response.json();
+  })
+  .then((bookData) => {
+  
+    renderBooks(bookData);
+  })
+  .catch((error) => {
+ 
+    console.error('Error fetching or rendering books:', error);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+fetchBooks();
+
